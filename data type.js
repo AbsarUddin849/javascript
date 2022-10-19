b611394d8ec60c57f27274081a131ab6a73c9ef1
absar uddin <absaruddin849@gmail.com>
2022-10-19 20:39:47 +0100

    function 1st class

---
+++ data type.js
@@ -0,0 +1,41 @@
+
+add2(10,40,10);
+
+// create function and 3 parameter for adding and devide for multiplication
+function acc(accountname,number){
+  result=accountname +number;
+console.log(result)
+}
+acc("Abdur Rahim",10897766464);
+// create function and 4 parameter for  and devide for multiplication
+function avg1(num1,num2,num3,num4){
+  multiplication=num1*num2*num3*num4;
+  result=multiplication/4;
+  console.log(result)
+}
+num1=10;num2=20;num3=30; num4=40;
+avg1(num1,num2,num3,num4);
+
+// create function and 4 parameter for  and devide for multiplication
+
+function avg (num1,num2,num3,num4){
+  // adding 4 numbers and find their average
+  addition=num1+num2+num3+num4;
+  result1=addition/4;
+  
+  console.log(result1)
+  // multiplying  4 numbers and find their average
+  multiplication=num1*num2*num3*num4;
+  result=multiplication/4;
+  console.log(result)
+   // subtraction  2 numbers and find modulas
+   pos=num4+num3;
+   neg=num1+num2;
+   res=pos-neg;
+   console.log(res)
+   modulas=res%4;
+   console.log(modulas)
+
+}
+num1=10;num2=20;num3=30; num4=40;
+avg(num1,num2,num3,num4);
